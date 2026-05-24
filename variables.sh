b540145fd1b1fef443f248349bb9fa4110ed1dfc
shiva reddy <mallikagajjala409@gmail.com>
2026-05-24 22:11:32 +0530

    variables.sh file is added final one

--- variables.sh
+++ variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash/
 
-AGE=27
+#!/bin/bash
+
+age=23
 name=shiva
 CITY=warangal
 
 echo "My name is $name, I am $AGE years old and I live in $CITY."
+echo "I should wait $((60 - $age)) years to reach sixty."
